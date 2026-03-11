24647cff5eefd24b6ec3f55fb866892db629ab48
Zomaddy <zosia.stachon@wp.pl>
2026-03-11 17:59:08 +0100

    ramp_wave

--- lab2/lab2_2.jl
+++ lab2/lab2_2.jl
@@ -6,7 +6,7 @@ fi = pi #przesunięcie fazowe
 t0 = 5.0 #czas pierwszej próbki
 tn = 10.0 #czas ostatniej próbki
 
-N = (tn - t0)*fs + 1 #liczba próbek, +1 jest bo dodajemy obie skrajne próbki czyli 5s i 10s
+N = (tn - t0)*fs + 1 #po prostu przekształcasz wzór z poprzedniego zadania
 
 t = t0 .+ (0:N-1) .* Ts #wektor czasów próbek
 
--- lab2/lab2_3.jl
+++ lab2/lab2_3.jl
@@ -1,2 +1,17 @@
-function ramp_wave(t; A = 1.0, T = 1.0, band = 20.0)
-    
+using CairoMakie
+function ramp_wave(t)
+    A = 1
+    T = 1
+    c = 0
+    a = 2
+
+    return mod.(a .* (t .+ 0.5*T), 2A) .- A .+ c
+
+end
+
+    t = range(0.0, 3.0, step = 0.01)
+
+    ramp_wave(t)
+    lines(t,ramp_wave(t))
+
+
