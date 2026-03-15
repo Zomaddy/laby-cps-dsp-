b1dd0d867fe95d940fe527196d08cf03bf3b352d
Zomaddy <zosia.stachon@wp.pl>
2026-03-15 12:20:28 +0100

    triangular wave

--- lab2/lab2_5.jl
+++ lab2/lab2_5.jl
@@ -1,9 +1,15 @@
 using CairoMakie
-function triangular_wave()
+function triangular_wave(t)
     A = 1
     T = 1
     c = 0
     a = 4
 
+    return A .* (1 - a .* abs.(mod(t ./ T + 0.25, A)-0.5))
+
 end
 
+    t = range(0.0, 3.0, step = 0.01)
+
+    triangular_wave.(t)
+    lines(t, triangular_wave.(t))
