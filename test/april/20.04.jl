defdbf398dd31927ce4389172759c96dd8b001b6
Zomaddy <zosia.stachon@wp.pl>
2026-06-10 12:42:48 +0200

    band-stop filter 46 with triangular window

--- test/april/20.04.jl
+++ test/april/20.04.jl
@@ -6,46 +6,30 @@ function rozwiazanie(;
     z::Vector{Int} = [35, 38, 46],
     )
 
-    dl_filtru = order + 1       #liczba współczynników, zależność dla filtru FIR
+    R = order       #rząd filtru
+    M = R / 2       #środek filtru
+    fc1 = f1 / fp
+    fc2 = f2 / fp       #normalizacja częstotliwości
+    sum_h = 0.0
 
-    h = zeros(Float64, dl_filtru)       #tworzenie wektora współczynników
+    for i in z
+        n = i - 1 - M           
 
-    w1 = 2*pi*f1 / fp
-    w2 = 2*pi*f2 / fp       #normalizacja częstotliwości
-
-    alfa = order/2.0        #wyznaczenie środka symetrii filtru (faza ma zmieniać się w sposób liniowy)
-
-    for i in 1:dl_filtru
-        n = i - 1           #zmiana indeksowania (od 0 a nie od 1)
-
-        w = 1.0 - abs.((n - alfa)/alfa)     #trójkątne okno Barletta
-
-        if n == alfa
-            h_s = 1.0 - (w2-w1)/pi      #idealna odp impulsowa, przypadek środkowego elementu
-
+        if n == 0.0
+            h_ideal = 1.0 - 2.0 * (fc2 - fc1)
         else
-            h_s = (sin(w1*(n-alfa)) - sin(w2*(n-alfa)))/(pi*(n-alfa))   #dla pozostałych współczynników (idealny filtr pasmowozaporowy)
-
+            h_ideal = (sin(2.0 * pi * fc1 * n) - sin(2.0 * pi * fc2 * n)) / (pi * n)
         end
-            h[i] = h_s * w      #nałożenie okna
+
+        w = 1.0 - abs.(n / M)     #okno trójkątne (Bartletta)
+
+        h = h_ideal * w         #nałożenie okna
+        sum_h += h
     end
 
-    suma = sum(h)       #dla filtru FIR suma współczynników = wzmocnienie dla 0 Hz (DC)
-
-    for j in 1:dl_filtru
-        h[i] = h[i]/suma        #dzięki temu suma wszystkich współczynników wynosi 1 -> sygnał stały przechodzi bez zmiany amplitudy, filtr ma wzmocnienie 1 w paśmie przepustowym
-    end
-
-    suma_final = sum(h[z])      #suma wybranych współczynników
-
-    return suma_final
-
+        return sum_h
 end
 
+wynik = rozwiazanie()
+println(wynik)
 
-
-
-
-
-
-end
