b350dd89993a5f20a51645415235ceacf9a06829
Zomaddy <zosia.stachon@wp.pl>
2026-06-14 11:55:34 +0200

    Whittaker-Shannon 77 samples

---
+++ test/test.jl
@@ -0,0 +1,29 @@
+function rozwiazanie(;
+    b::Vector{Float64} = [0.15704537558097117, -0.6289713606967544, 1.1370016882066123, -1.1370016882066125, 0.6289713606967546, -0.1570453755809712],
+    a::Vector{Float64} = [1.0, -1.0098233508496246, 1.4741171183117325, -0.30770374237850595, 0.25583960213116463, 0.20144696470235185],
+    F::Vector{Float64} = [0.26, 0.31, 0.43],
+)
+    sum_phase = 0.0
+
+    for f in F
+        num = 0.0 + 0.0*im
+    
+        for l in 1:length(b)
+            num += b[l] * exp(-2.0 * im * pi * f * (l-1))
+        end
+
+        denum = 0.0 + 0.0*im
+
+        for m in 1:length(a)
+            denum += a[m] * exp(-2.0 * im * pi * f * (m-1))
+        end
+
+        H_z = num / denum
+        sum_phase += angle(H_z)
+    end
+
+    return sum_phase / length(F)
+end
+
+wynik = rozwiazanie()
+println(wynik)
